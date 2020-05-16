db15d767ef6d8cd64a1f1136ccbf379ac8bf2c33
OMAAB <omar.abdi.ali@ist.com>
2020-05-16 22:20:55 +0200

    deleted billingservice made the acces allowed somehow, commented out getmapping, made some changes in vueapp

--- src/components/Billings.vue
+++ src/components/Billings.vue
@@ -27,7 +27,7 @@
                         </select>
                     </td>
                     <td>
-                        {{  }}: SEK</td>
+                        <input type="number" id="result" v-model="result">: SEK</td>
                 </tr>
                 </tbody>
             </table>
@@ -40,7 +40,6 @@
     </div>
 </template>
 <script>
-    import BillingService from "../service/BillingService";
     export default {
         name: "calculate",
         data() {
@@ -53,28 +52,50 @@
                     'Allmän förskola 3-5år, max 15 tim'],
                 message: "",
                 income: "",
+                barn1Typ: "",
+                barn2Typ: "",
+                barn3Typ: "",
+                amountBarn1: "",
+                amountBarn2: "",
+                amountBarn3: "",
                 errors: []
             }
         },
         computed: {
             id() {
                 return this.$route.params.id;
+            },
+            result: function(){
+                let maxAmount = 40000;
+                let barnTyp1 = 0.02;
+                let barnTyp2 = 0.04;
+                if (this.income < maxAmount){
+                    return this.income * barnTyp1;
+                } else if (this.income > maxAmount) {
+                    return this.income * barnTyp2;
+                }
+                return 0;
             }
         },
         methods: {
             calculate() {
+                /**
+                 *
+                 */
                 console.log(this.income);
                 console.log(this.barn1Typ);
                 console.log(this.barn2Typ);
                 console.log(this.barn3Typ);
+                console.log(this.amountBarn1);
+                console.log(this.amountBarn2);
+                console.log(this.amountBarn3);
                 const exampleData = {
                     income: this.income,
-                    barn1Typ: this.barn1Typ  * this.income,
-                    barn2Typ: this.barn2Typ  * this.income,
-                    barn3Typ: this.barn3Typ  * this.income
+                    barn1Typ: this.barn1Typ,
+                    barn2Typ: this.barn2Typ,
+                    barn3Typ: this.barn3Typ,
                 };
-
-                fetch('https://jsonplaceholder.typicode.com/posts', {
+                fetch('http://localhost:8080/calculate-sum', {
                     method: "POST", // or 'PUT'
                     headers: {
                         "Content-Type": "application/json"
@@ -83,34 +104,16 @@
                 })
                     .then(response => response.json())
                     .then(data => {
-                        console.log("Success:", data);
+                        this.income = data.income;
+                            //this.amountBarn1 = data.amountBarn1,
+                            //this.amountBarn2 = data.amountBarn2,
+                            //this.amountBarn3 = data.amountBarn3
+                            console.log("Success:", exampleData, data);
                     })
                     .catch(error => {
                         console.error("Error:", error);
                     });
             },
-            postBillingDetails(){
-                BillingService.createBilling(this.name).then(res =>{
-                    this.income = res.data.income;
-                    this.barn1Typ = res.data.barn1Typ;
-                    this.barn2Typ = res.data.barn2Typ;
-                    this.barn3Typ = res.data.barn3Typ;
-                }).then(() =>{
-                    this.$router.push('/calculate-sum');
-                });
-            },
-            refreshBillingDetails() {
-                BillingService.retrieveBillings(this.name).then(res => {
-                    this.income = res.data.income;
-                    this.barn1Typ = res.data.barn1Typ;
-                    this.barn2Typ = res.data.barn2Typ;
-                    this.barn3Typ = res.data.barn3Typ;
-                });
-            }
         },
-        created() {
-            this.refreshBillingDetails();
-            this.postBillingDetails();
-        }
     };
 </script>
